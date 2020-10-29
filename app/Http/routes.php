40959083a6bf8d28261f917f019a642a5521494d
Albert Med <albert91.me.d@gmail.com>
2020-10-28 18:12:31 -0600

    prueba impr 1.4

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -88,15 +88,15 @@ route::get('print', function () {
         
         file_put_contents($fileName, $zpl_code);
         //lpr -P 'Zebra_Technologies_ZTC_GC420t_EPL_itk_gna_ubt_200' -o raw ''
-        echo "lpr -P 'Zebra_Technologies_ZTC_GC420t_' -o raw media=Custom.4x6in -o page-left=0 -o page-right=0 -o page-top=0 -o page-bottom=0 '" . $fileName . "'";
-        exec("lpr -P 'Zebra_Technologies_ZTC_GC420t_' -o raw media=Custom.4x6in -o page-left=0 -o page-right=0 -o page-top=0 -o page-bottom=0 '". $fileName."'");
+        echo "lp -d 'Zebra_Technologies_ZTC_GC420t_' -o raw media=Custom.4x6in '" . $fileName . "'";
+        exec("lp -d 'Zebra_Technologies_ZTC_GC420t_' -o raw media=Custom.4x6in '". $fileName."'");
          //lpr -P 'Zebra_Technologies_ZTC_GC420t_' -o raw media=Custom.4x6in -o page-left=0 -o page-right=0 -o page-top=0 -o page-bottom=0 '' 
     //Zebra-Technologies-ZTC-GC420t-(EPL)
     } catch (\Exception $e) {
         echo $e->getMessage();
     }
 
-    echo 'hecho';
+    echo '      terminado...';
 });
 
 Route::post('cambio.password', 'Mod00_AdministradorController@cambiopassword');
