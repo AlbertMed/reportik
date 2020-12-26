9774cda765db228ecf54a202a1cfbbda15f2988e
Albert Med <albert91.me.d@gmail.com>
2020-12-25 19:08:17 -0600

    kardex por OV PDF

--- app/Http/Controllers/Mod_RPTFinanzasController.php
+++ app/Http/Controllers/Mod_RPTFinanzasController.php
@@ -134,10 +134,83 @@ class Mod_RPTFinanzasController extends Controller
     }
     public function KardexOV(Request $request){
         //dd(Input::get('pKey'));
+    $Id_OV = Input::get('pKey');
+        $info = DB::select("SELECT CLI_CodigoCliente + ' - ' + CLI_RazonSocial AS CLIENTE,                                  
+        PRY_CodigoEvento + ' - ' + PRY_NombreProyecto AS PROYECTO,
+		CCON_Nombre as COMPRADOR
+		FROM OrdenesVenta                                
+    INNER JOIN Clientes ON OV_CLI_ClienteId = CLI_ClienteId
+    LEFT  JOIN Proyectos ON OV_PRO_ProyectoId = PRY_ProyectoId AND PRY_Activo = 1 AND PRY_Borrado = 0
+	INNER JOIN ClientesContactos ON OV_CCON_ContactoId = CCON_ContactoId AND CCON_Eliminado = 0
+
+    where OV_OrdenVentaId = ?",[$Id_OV]);
+    //dd($info);
         if (Auth::check()) {
-            $data= '';
-            //$pdf = \PDF::loadView('Finanzas.kardexOV', compact('data'));
-            $pdf = \PDF::loadView('welcome', compact('data'));
+            $sql = "SELECT (Select Cast(OV_FechaOV as Date) from OrdenesVenta Where OV_OrdenVentaId =  '". $Id_OV . "' ) as FECHA,
+        'VENTAS' as IDENTIF,
+       (Select OV_CodigoOV from OrdenesVenta Where OV_OrdenVentaId =  '" . $Id_OV . "' ) as DOCUMENT, 
+       (Select OV_ReferenciaOC from OrdenesVenta Where OV_OrdenVentaId =  '". $Id_OV ."' ) as REFERENCIA,       
+       SUM(OVD_CantidadRequerida * OVD_PrecioUnitario - (OVD_CantidadRequerida * OVD_PrecioUnitario * ISNULL(OVD_PorcentajeDescuento, 0.0)) +
+       ((OVD_CantidadRequerida * OVD_PrecioUnitario) - (OVD_CantidadRequerida * OVD_PrecioUnitario * ISNULL(OVD_PorcentajeDescuento, 0.0))) *
+       ISNULL(OVD_CMIVA_Porcentaje, 0.0)) as IMP_OV,
+         0 as IMP_FAC,
+         0 as IMP_EMB,
+         0 as IMP_PAG
+From OrdenesVentaDetalle
+Where OVD_OV_OrdenVentaId =  '". $Id_OV ."' 
+Union All
+Select  Cast(FTR_FechaFactura as Date) as FECHA,
+        'FACTURAS' as IDENTIF,
+        FTR_NumeroFactura as DOCUMENT,
+        (Select CMM_Valor from ControlesMaestrosMultiples Where CMM_ControlId = FTR_CMM_TipoRegistroId) as REFERENCIA,
+        0 as IMP_OV,
+        SUM(FTRD_CantidadRequerida * FTRD_PrecioUnitario -                
+        FTRD_CantidadRequerida * FTRD_PrecioUnitario * ISNULL(FTRD_PorcentajeDescuento, 0.0) +
+        ((FTRD_CantidadRequerida * FTRD_PrecioUnitario) - (FTRD_CantidadRequerida * FTRD_PrecioUnitario * 
+        ISNULL(FTRD_PorcentajeDescuento, 0.0))) * ISNULL(FTRD_CMIVA_Porcentaje, 0.0)) as IMP_FAC,
+         0 as IMP_EMB,
+         0 as IMP_PAG   
+From Facturas                
+Inner join FacturasDetalle on FTRD_FTR_FacturaId = FTR_FacturaId  
+Where FTR_Eliminado = 0 and FTR_OV_OrdenVentaId =  '". $Id_OV ."'  
+Group By FTR_FechaFactura, FTR_NumeroFactura, FTR_CMM_TipoRegistroId
+Union All
+Select  Cast(CXCP_FechaCaptura as Date) as FECHA,
+        'PAGOS' as IDENTIF,
+        CXCP_IdentificacionPago as DOCUMENT,
+        (Select CMM_Valor from ControlesMaestrosMultiples Where CMM_ControlId = '42B7AE60-A156-4647-A85B-56581A74B2B8') as REFERENCIA,
+        0 as IMP_OV,
+        0 as IMP_FAC,
+        0 as IMP_EMB,
+        (CXCP_MontoPago * CXCP_MONP_Paridad) as IMP_PAG
+From CXCPagos   
+Inner Join CXCPagosDetalle on CXCP_CXCPagoId = CXCPD_CXCP_CXCPagoId   
+Inner Join Facturas on FTR_FacturaId = CXCPD_FTR_FacturaId and FTR_OV_OrdenVentaId =  '". $Id_OV ."' 
+Union All
+Select  Cast(NC_FechaPoliza as Date) as FECHA,
+        'NOTA CREDITO' as IDENTIF,
+        NC_Codigo as DOCUMENT,
+        NCD_Descripcion as REFERENCIA,
+        0 as IMP_OV,
+        0 as IMP_FAC,
+        0 as IMP_EMB,
+        (CXCP_MontoPago * CXCP_MONP_Paridad) as IMP_PAG
+From CXCPagos
+Inner Join CXCPagosDetalle on CXCP_CXCPagoId = CXCPD_CXCP_CXCPagoId   
+inner Join NotasCredito on NC_NotaCreditoId = CXCPD_NC_NotaCreditoId
+inner join NotasCreditoDetalle on NCD_NC_NotaCreditoId = NC_NotaCreditoId
+inner join Facturas on NC_FTR_FacturaId = FTR_FacturaId and FTR_OV_OrdenVentaId =  '". $Id_OV ."' 
+
+Order by FECHA, IDENTIF DESC";
+            $ovs = DB::select($sql);
+            $sumOV = array_sum(array_pluck($ovs, 'IMP_OV'));
+            $sumFAC = array_sum(array_pluck($ovs, 'IMP_FAC'));
+            $sumEMB = array_sum(array_pluck($ovs, 'IMP_EMB'));
+            $sumPAG = array_sum(array_pluck($ovs, 'IMP_PAG'));
+           // $ovs = collect($ovs);
+            $pdf = \PDF::loadView('Finanzas.kardexOV_PDF', 
+            compact('ovs', 'info', 'sumOV', 'sumFAC', 'sumEMB', 'sumPAG'));
+           // $pdf = \PDF::loadView('welcome', compact('data'));
             $pdf->setPaper('Letter', 'landscape')->setOptions(['isPhpEnabled' => true]); 
             return $pdf->stream('Kardex OV ' . ' - ' . date("d/m/Y") . '.Pdf');
         } else {
---
+++ resources/views/Finanzas/kardexOV_PDF.blade.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Kardex por OV</title>
+    <style>
+        /*
+                Generic Styling, for Desktops/Laptops
+                */
+table { width: 100%; border-collapse: collapse; font-family: arial;
+ } th { font-weight: bold;
+font-family: 'Helvetica'; font-size: 12px; background-color: #b0adad; } 
+td { font-family: 'Helvetica'; font-size: 11px; }
+
+img {
+width: 13%;
+height: 13%;
+position: absolute;
+margin-left: 5px;
+margin-top: -6px;
+}
+h5 { font-family: 'Helvetica';
+margin-bottom: 2px; margin-top: 0; } 
+     #header { position: fixed; margin-top: 2px; }
+        #content {
+            position: relative;
+            top: 10%
+        }
+         table,
+        th,
+        td {
+            
+            border: .5px solid black;
+           
+            
+        }
+        .numbers{
+            text-align: center;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        .zrk-silver{
+            
+            color: black;
+        }
+        .zrk-dimgray{
+            background-color: #514d4a;
+            color: white;
+        }
+        .zrk-gris-claro{
+            background-color: white;
+            color: black;
+        }
+        .zrk-silver-w{
+    
+        }
+        .table > thead > tr > th, 
+        .table > tbody > tr > th, 
+        .table > tfoot > tr > th, 
+        .table > thead > tr > td, 
+        .table > tbody > tr > td,
+        .table > tfoot > tr > td { 
+            padding-bottom: 2px; padding-top: 2px; padding-left: 4px; padding-right: 0px;
+        }
+        .total{
+            text-align: right; 
+            padding-right:4px;
+        }
+        h2,h3 ,h4{
+            
+            padding: 0px;
+            margin: 0px;
+            
+        }
+    </style>
+</head>
+
+<body>
+
+    <div id="header">
+        <img src="images/logo.png">
+        <!--empieza encabezado, continua cuerpo-->
+        <table border="1px" class="table table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <td colspan="6" align="center" bgcolor="#fff">
+                       
+                        <h2><b>Kardex de Estado de Cuenta por OV</b></h2>
+                       @if(count($ovs)>0)
+                        <h3>PROYECTO: {{$info[0]->PROYECTO}}</h3>
+                        <h3>CLIENTE: {{$info[0]->CLIENTE}}</h3>
+                        <h3>CONTACTO: {{$info[0]->COMPRADOR}}</h3>
+                        @endif
+                    </td>
+
+                </tr>
+            </thead>
+        </table>
+       
+    </div>
+        <!--Cuerpo o datos de la tabla-->
+        <div id="content">
+           @if(count($ovs)>0)
+            <div class="row">
+               
+                <div class="col-md-8">                               
+                        <?php 
+                            $index = 1; ?>
+                           
+                                                                                  
+                         
+                           <table class="table table-striped" style="table-layout:fixed;">
+                            <thead class="table-condensed">
+                                <tr style="width:100%">
+                                    <th style="width:56%" class="total">{{'TOTAL: .'}}</th>
+                                    
+                        
+                                    <th style="width:11%" class="numbers">${{number_format($sumOV,'2', '.',',')}}</th>
+                                    <th style="width:11%" class="numbers">${{number_format($sumFAC,'2', '.',',')}}</th>
+                                    <th style="width:11%" class="numbers">${{number_format($sumEMB,'2', '.',',')}}</th>
+                                    <th style="width:11%" class="numbers">${{number_format($sumPAG,'2', '.',',')}}</th>
+                                </tr>
+                        
+                            </thead>
+                        </table>
+                           <table class="table table-striped" style="table-layout:fixed;">            
+                                <thead  class="table-condensed">
+                                    <tr style="width:100%">
+                                        <th style="width:3%">#</th>
+                                        <th style="width:10%">FECHA</th>
+                                        <th style="width:11%">IDENTIFICADOR</th>
+                                        <th style="width:10%">DOCUMENTO</th>
+                                        <th style="width:22%">REFERENCIA</th>
+                                        
+                                        <th style="width:11%">IMP. OV</th>
+                                        <th style="width:11%">IMP. FACTURA</th>
+                                        <th style="width:11%">IMP. EMBARQUE</th>
+                                        <th style="width:11%">IMP. PAGO</th>
+                                    </tr>
+                                </thead>  
+                            </table>  
+
+                            
+                            
+                            @foreach ($ovs as $rep) 
+
+                            <table class="table table-striped" style="table-layout:fixed;">
+                                <tbody>
+                                    <tr>
+                                        <td style="width:3%" class="zrk-silver-w" scope="row">
+                                        {{$index}}
+                                        </td>
+                                        <td style="width:10%" class="zrk-silver-w" scope="row">
+                                            {{date_format(date_create($rep->FECHA), 'd/m/Y')}}
+                                        </td>
+                                        <td style="width:11%" class="zrk-silver-w" scope="row">
+                                            {{$rep->IDENTIF}}
+                                        </td>
+                                        <td style="width:10%" class="zrk-silver-w" scope="row">
+                                            {{$rep->DOCUMENT}}
+                                        </td>                                        
+                                        <td style="width:22%" class="zrk-silver-w" scope="row">
+                                            {{$rep->REFERENCIA}}
+                                        </td>
+                                        <td style="width:11%" class="zrk-silver-w numbers" scope="row">
+                                            ${{number_format($rep->IMP_OV,'2', '.',',')}}
+                                        </td>
+                                        <td style="width:11%" class="zrk-silver-w numbers" scope="row">
+                                            ${{number_format($rep->IMP_FAC,'2', '.',',')}}
+                                        </td>
+                                        <td style="width:11%" class="zrk-silver-w numbers" scope="row">
+                                            ${{number_format($rep->IMP_EMB,'2', '.',',')}}
+                                        </td>
+                                        <td style="width:11%" class="zrk-silver-w numbers" scope="row">
+                                            ${{number_format($rep->IMP_PAG,'2', '.',',')}}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <?php 
+                                                        $index++; ?>
+                            @endforeach    
+                 
+                </div>
+            
+            </div>
+        @endif 
+           
+          
+        </div>
+                <footer>
+                    
+                    <script type="text/php">
+                        $font = $fontMetrics->get_font("Arial, Helvetica, sans-serif","normal"); 
+
+                       
+                        $date = 'Fecha de impresion:  <?php echo $hoy = date("d-m-Y H:i:s"); ?>';
+                        $text = 'Pagina: {PAGE_NUM} / {PAGE_COUNT}'; 
+                        $tittle = 'KardexOV.Pdf'; 
+                        
+                        $pdf->page_text(580, 23, $date, $font, 9);  
+
+                        $pdf->page_text(35, 580, $text, $font, 9);                         
+                        $pdf->page_text(630, 580, $tittle, $font, 9);                                                 
+                    </script>
+                </footer>
+              
+</body>
+
+</html>
