5819568b7c50604790dd2bd46d2a6419c582da6e
Albert Med <albert91.me.d@gmail.com>
2023-02-14 16:05:19 -0600

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -335,8 +335,9 @@ Route::group(['prefix' => 'home/FINANZAS'], function () {
     Route::get('consultaDatosCalendariosCXPResumen2', 'FlujoEfectivoController@consultaDatosCalendariosCXPResumen2');    
 */
 });
-//Route::any('reporte-comprasFicha-exportar', 'Mod_FinanzasController@exportar')->name('reporte-comprasFicha-exportar');
-
+Route::any('reporte-comprasFicha-exportar', 'Mod_FinanzasController@exportar')->name('reporte-comprasFicha-exportar');
+Route::any('almacen-digital-pro-exportar', 'Mod_FinanzasController@exportar_factura')->name('almacen-digital-pro-exportar');
+//Route::any('almacen-digital-pro-exportar', 'Mod_FinanzasController@exportar_factura');
 //Rutas del Módulo de inventarios
 Route::get('admin/altaInventario', 'Mod00_AdministradorController@altaInventario');
 Route::post('admin/altaInventario', 'Mod00_AdministradorController@altaInventario2');
